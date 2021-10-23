241d20dd9e051c0b1ebf148e754547b2cac8019b
sc0der <siriuspoger@gmail.com>
2021-10-23 15:32:08 +0500

    init

---
+++ asyncioCoding.py
@@ -0,0 +1,57 @@
+import time
+import asyncio
+
+async def boil_water(sec):
+    print(f"Start boiling water for {sec} seconds")
+    await asyncio.sleep(sec)
+    # time.sleep(sec)
+    print(f"End boiling water for {sec} seconds")
+
+async def washing_machine(sec):
+    print("Start washing machine")
+    await asyncio.sleep(sec)
+    # time.sleep(sec)
+    print("End washing machine")
+
+
+async def dryer(sec):
+    print("Start dryer")
+    await asyncio.sleep(sec)
+    # time.sleep(sec)
+    print("End dryer")
+
+
+async def dishwasher(sec):
+    print("Start dishwasher")
+    await asyncio.sleep(sec)
+    # time.sleep(sec)
+    print("End dishwasher")
+
+async def clean_potatoes(pieces):
+    print("Start cleaning potatoes")
+    for ix in range(pieces):
+        print(f"Cleaning potato {ix}") 
+        time.sleep(0.5)
+        # await asyncio.sleep(0.5)
+    print("End cleaning potatoes")
+
+# def main():
+#     dishwasher(3)
+#     washing_machine(3)
+#     dryer(3)
+#     boil_water(4)
+#     clean_potatoes(14)
+
+async def main():
+    await asyncio.gather(dishwasher(3), washing_machine(3), boil_water(4), clean_potatoes(14))
+
+# start = time.time()
+# main()
+# end = time.time()
+# print(f"Elapsed {end-start}")
+
+
+start = time.time()
+asyncio.run(main())
+end = time.time()
+print(f"Elapsed {end-start}")
